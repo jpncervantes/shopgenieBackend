44bec649ee885f07817f0147c8e16eae0df8081b
jpncervantes <johnpaulncervantes@gmail.com>
2025-01-23 09:54:40 +0800

    removed / on allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,6 @@ const allowedOrigins = [
   'https://localhost:3500',
   'http://localhost:3000',
   'http://127.0.0.1:5500',
-  'https://shopgeniefrontend.onrender.com/'
+  'https://shopgeniefrontend.onrender.com'
 ]
 module.exports = allowedOrigins
